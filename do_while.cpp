0890eef913b1fff70d41e474b6f70a48e942d732
GEOKV <ge-obany@hotmail.com>
2023-03-15 00:21:24 -0600

    ejercios

---
+++ do_while.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main(){
+	char respuesta;
+	do{
+		cout<<"desea ingresar otro valor (s/n) ";
+		cin>>respuesta;
+		
+		}while(respuesta=='s' || respuesta== 'S');
+		
+	
+	system("pause");
+}
